a5905ead7a13019d338e935d0d02d4da1b0b35a7
“mosaic0762” <571522671@qq.com>
2021-08-17 17:29:57 +0800

    添加文件

---
+++ untitled222/src/test/java/com/atguigu/git.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author mosaic
+ * @date 2021/8/17-17:19
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
