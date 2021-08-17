ecc62e467d8a3b7e804505172aa1d9884af64ab6
“mosaic0762” <571522671@qq.com>
2021-08-17 17:40:24 +0800

    更新4创建分支

--- untitled222/src/test/java/com/atguigu/git.java
+++ untitled222/src/test/java/com/atguigu/git.java
@@ -8,5 +8,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("更新1!");
+        System.out.println("创建分支!");
     }
 }
